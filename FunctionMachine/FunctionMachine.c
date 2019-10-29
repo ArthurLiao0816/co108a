c8274ab183e59ded45ac7ef37445a12d509fba2e
ArthurLiao0816 <adamliao0816@yahoo.com>
2019-10-30 03:11:05 +0800

    RAM8.hdl

--- FunctionMachine/FunctionMachine.c
+++ FunctionMachine/FunctionMachine.c
@@ -4,6 +4,6 @@ int main()
 {
     int i;
     for(i=1;i<=8;i++){
-        printf("Mux8Way16(a=out%d,b=out%d,c=out%d,d=out%d,e=out%d,f=out%d,g=out%d,h=out%d,sel=address[3..5],out=oout%d);\n",8*i-7,8*i-6,8*i-5,8*i-4,8*i-3,8*i-2,8*i-1,8*i-0,i);
+        printf("RAM4K(in=in,load=s%d,address=address[0..10],out=out%d);\n",i,i);
     }
 }
